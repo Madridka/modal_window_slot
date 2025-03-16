86380d6e6f68424f9d10804a0bcd993152c68d6a
Madridka <serbin_kirill1807@mail.ru>
2025-03-16 23:44:31 +0700

    -clear App.vue -added ModalWindow.vue -in modalWindow created slot with Default content, added some default config -in app.vue added slot "ModalWindow" and import this components -added button @click="isModalOpen = true"

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <button @click="isModalOpen = true"></button>
+    <ModalWindow>
+
+    </ModalWindow>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ModalWindow from "./components/ModalWindow.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    ModalWindow,
+  },
+  data() {
+    return {
+      isModalOpen: false,
+    };
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/ModalWindow.vue
@@ -0,0 +1,22 @@
+<template>
+  <div :v-if="isVisible = true">
+    <div :is-visible="isModalOpen">
+      <slot>Default content</slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ModalWindow",
+  props: {
+    isModalOpen: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style>
+</style>
