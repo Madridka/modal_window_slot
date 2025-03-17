2c6a1f0a2f79b4858e37192018b4fe740905bee6
Madridka <serbin_kirill1807@mail.ru>
2025-03-17 18:14:05 +0700

    added slot, fuctionals -added button with isModalOpen -props going to ModalWindow from :is-visible="isModalOpen", and named isVisible (type:bool) -from ModalWIndow.vue emit "close" going to App.vue, @close="isModal=false", and modal window is going to close

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
-    <button @click="isModalOpen = true"></button>
-    <ModalWindow>
-
+    <button @click="isModalOpen = true">Открыть модальное окно</button>
+    <ModalWindow :is-visible="isModalOpen" @close="isModalOpen = false">
+      <h1>Контент модального окна</h1>
+      <h3>Если в родительском компоненте тут ничего не будет, то отразиться контент 
+        в дочернем компоненте в slot, а именно "Тут контент"</h3>
     </ModalWindow>
   </div>
 </template>
--- src/components/ModalWindow.vue
+++ src/components/ModalWindow.vue
@@ -1,7 +1,8 @@
 <template>
-  <div :v-if="isVisible = true">
-    <div :is-visible="isModalOpen">
-      <slot>Default content</slot>
+  <div class="modal" v-if="isVisible">
+    <div class="modal-content">
+      <button @click="closeModal">Close</button>
+      <slot>Тут контент</slot>
     </div>
   </div>
 </template>
@@ -10,13 +11,26 @@
 export default {
   name: "ModalWindow",
   props: {
-    isModalOpen: {
+    isVisible: {
       type: Boolean,
       default: false,
     },
   },
+  methods: {
+    closeModal() {
+      this.$emit("close");
+    }
+  }
 };
 </script>
 
 <style>
+.modal {
+  max-width: 400px;
+  background: rgba(121, 117, 117, 0.5);
+  display: flex;
+  align-content: center;
+  justify-content: center;
+}
+
 </style>
